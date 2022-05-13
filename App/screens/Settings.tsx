143b40cd60af52fce132a02b3ba40e54bef5ae2a
David Heinebäck <du222aa@student.lnu.se>
2022-05-13 15:55:13 +0200

    add Button compoent and map all Resolutions

--- App/screens/Settings.tsx
+++ App/screens/Settings.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { ContextObject, Resolution } from '../components/Context'
+import Button from '../components/Button'
 
 function Settings() {
+  const { resolution, setResolution } = React.useContext(ContextObject)
+
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={styles.container}>
+      <Text>Choose resolution</Text>
+      {Object.entries(Resolution).map(([key, value], index) => {
+        return (
+          <Button
+            key={index}
+            value={key}
+            onPress={() => setResolution(value)}
+            active={value === resolution}
+          />
+        )
+      })}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+})
 
 export default Settings
