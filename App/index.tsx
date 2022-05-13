d2a90aef0e473a9f6d68681bd02ed48089802491
David Heinebäck <du222aa@student.lnu.se>
2022-05-13 15:53:31 +0200

    add context

--- App/index.tsx
+++ App/index.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View, StatusBar } from 'react-native'
 import Navigation from './config/Navigation'
+import Context from './components/Context'
 
 export default function App() {
   return (
-    <>
+    <Context>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
       <Navigation />
-    </>
+    </Context>
   )
 }
 
